422fa6b5c2600082eeb7e4b1cc3c605ba6d2255b
Geff <geff1982@gmail.com>
2020-10-28 16:46:14 +0100

    Init commit

---
+++ src/main/java/com/safetynet/alerts/SafetyNetAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyNetAlertsApplication.class, args);
+	}
+
+}
